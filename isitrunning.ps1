f93b4d90726f507b263f6440caab27b8e5a8f51e
Dylan MacIntyre <dmac0822@gmail.com>
2022-08-09 08:47:27 -0400

    initial commit

---
+++ isitrunning.ps1
@@ -0,0 +1,7 @@
+﻿#check to see if a service is running
+#ask which what the service is
+$theservice = Read-Host -Prompt 'Input your service name'
+#search for service
+$isitrunning = Get-Service -Name $theservice
+#output service is running or is not
+echo $isitrunning
